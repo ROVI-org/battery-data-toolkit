5fca28d2bffa59ea168a4e0144f30b6e85710fd9
Logan Ward <WardLT@users.noreply.github.com>
2020-10-20 10:06:29 -0500

    Correct setup.py to install *all* of the modules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='batdata',
     version='0.0.1',
-    packages=find_packages(include=('batdata.*',)),
+    packages=find_packages(),
     install_requires=['pandas'],
     entry_points={
         "console_scripts": ["batdata-convert=batdata.cli:main"],
