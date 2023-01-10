3ca4f05e34f106227ae119be2f538c43628c8e96
Logan Ward <ward.logan.t@gmail.com>
2023-01-10 13:59:47 -0500

    Test the ability to store and load cycle_stats

--- batdata/tests/test_data.py
+++ batdata/tests/test_data.py
@@ -12,12 +12,16 @@ from batdata.data import BatteryDataset
 
 @fixture()
 def test_df():
-    return BatteryDataset(raw_data=pd.DataFrame({
+    raw_data = pd.DataFrame({
         'test_time': [0, 1, 2.],
         'current': [1., 0., -1.],
         'voltage': [2., 2., 2.],
-        'other': [1, 2, 3]
-    }), metadata={'name': 'Test data'})
+        'other': [1, 2, 3],
+    })
+    cycle_stats = pd.DataFrame({
+        'cycle_number': [0],
+    })
+    return BatteryDataset(raw_data=raw_data, cycle_stats=cycle_stats, metadata={'name': 'Test data'})
 
 
 def test_write_hdf(tmpdir, test_df):
