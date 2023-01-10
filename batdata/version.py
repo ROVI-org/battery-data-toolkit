b930fbdb78430f1c681a29e1b20b9d6922cc4536
Logan Ward <ward.logan.t@gmail.com>
2023-01-10 11:52:19 -0500

    Switch project over to poetry

--- batdata/version.py
+++ batdata/version.py
@@ -1,1 +1,7 @@
-__version__ = '0.0.1'
+# we target 3.8+, so this should be okay without fallback to importlib_metadata
+import importlib.metadata
+
+# single source of truth for package version,
+# see https://packaging.python.org/en/latest/single_source_version/
+
+__version__ = importlib.metadata.version('battery-data-toolkit')
