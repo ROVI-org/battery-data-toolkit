baa22f05e45d6b5712f4c4b876ea4c9a22a01d87
Logan Ward <ward.logan.t@gmail.com>
2024-03-13 15:19:28 -0400

    Re-implement integrals and tests
    No more steps

--- tests/postprocess/test_integral.py
+++ tests/postprocess/test_integral.py
@@ -1,27 +1,58 @@
-from math import isclose
+from pathlib import Path
 
+from pytest import mark
 import numpy as np
-from pytest import fixture
 
 from batdata.data import BatteryDataset
-from batdata.extractors.arbin import ArbinExtractor
 from batdata.postprocess.integral import CapacityPerCycle, StateOfCharge
 
 
-@fixture
-def example_data(file_path) -> BatteryDataset:
-    ex_file = file_path / 'arbin_example.csv'
-    return ArbinExtractor().parse_to_dataframe([ex_file])
+def get_example_data(file_path: Path, from_charged: bool) -> BatteryDataset:
+    ex_file = file_path / 'example-data' / f'single-resistor-constant-charge_from-{"" if from_charged else "dis"}charged.hdf'
+    return BatteryDataset.from_batdata_hdf(ex_file)
 
 
-def test_cycle_stats(example_data):
+@mark.parametrize('from_charged', [True, False])
+def test_cycle_stats(file_path, from_charged):
+    example_data = get_example_data(file_path, from_charged)
     feat = CapacityPerCycle().compute_features(example_data)
-    assert np.isclose([0, 1], feat['cycle_number']).all()
-    assert np.isclose([-3.256, -3.262], feat['discharge_energy'], atol=1e-2).all()
-    assert np.isclose([-1.073, -1.074], feat['discharge_capacity'], atol=1e-2).all()
+    assert np.isclose([0], feat['cycle_number']).all()
+
+    # Capacity is the 1 A-hr
+    assert np.isclose([1.0], feat['discharge_capacity'], rtol=1e-2).all()
+    assert np.isclose([1.0], feat['charge_capacity'], rtol=1e-2).all()
+
+    # Energy to charge is (2.1 V + 3.1 V) / 2 * 1 A * 3600 s = 9360 J
+    # Energy produced during discharge is (1.9 V + 2.9 V) * 1 A * 3600 s = 8640 J
+    assert np.isclose([9360.], feat['charge_energy'], rtol=1e-3).all()
+    assert np.isclose([8640.], feat['discharge_energy'], rtol=1e-3).all()
 
 
-def test_capacity(example_data):
-    StateOfCharge().enhance(example_data.raw_data)
-    assert isclose(example_data.raw_data['capacity'].max(), 1.075, abs_tol=1e-1)
-    assert 'energy' in example_data.raw_data
+@mark.parametrize('from_charged', [True, False])
+def test_capacity(file_path, from_charged):
+    example_data = get_example_data(file_path, from_charged)
+    soc = StateOfCharge()
+    soc.enhance(example_data.raw_data)
+
+    assert all(c in example_data.raw_data for c in soc.column_names)
+    assert not any(example_data.raw_data[c].isna().any() for c in soc.column_names)
+
+    # First cell should be 0
+    assert np.isclose(example_data.raw_data.drop_duplicates('cycle_number', keep='first')[soc.column_names], 0).all()
+
+    # Last cell of the capacity should be zero for our test cases
+    assert np.isclose(example_data.raw_data['cycle_capacity'].iloc[-1], 0., atol=1e-3)
+
+    # The capacity for the first few steps should be I*t/3600s
+    first_steps = example_data.raw_data.iloc[:3]
+    current = first_steps['current'].iloc[0]
+    assert np.isclose(first_steps['cycle_capacity'], current * first_steps['test_time'] / 3600).all()
+
+    # The energy for the first few steps should be
+    #  discharging = I * \int_0^t (2.9 - t/3600) = I * (2.9t - t^2/7200)
+    #  charging = I * \int_0^t (2.1 + t/3600) = I * (2.1t + t^2/7200)
+    if from_charged:
+        answer = current * (2.9 * first_steps['test_time'] - first_steps['test_time'] ** 2 / 7200)
+    else:
+        answer = current * (2.1 * first_steps['test_time'] + first_steps['test_time'] ** 2 / 7200)
+    assert np.isclose(first_steps['cycle_energy'], answer, rtol=1e-3).all()
