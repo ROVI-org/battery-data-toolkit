8f80f6c5d0022f69715af4435d9d43a1c3934a5d
lward <lward@anl.gov>
2024-10-18 17:21:12 -0400

    Implement streaming by cycle

--- batdata/streaming.py
+++ batdata/streaming.py
@@ -1,10 +1,24 @@
 """Retrieve data in smaller chunks from a large HDF5 file"""
 from pathlib import Path
-from typing import Union, Iterator, Dict
+from contextlib import contextmanager
+from typing import Union, Iterator, Dict, List
 
 import pandas as pd
 from pandas import HDFStore
 from pandas.io.pytables import TableIterator
+
+from batdata.data import BatteryDataset
+
+
+@contextmanager
+def _get_raw_data_iterator_h5(hdf5_path: Union[Path, str, HDFStore], **kwargs) -> Iterator[pd.DataFrame]:
+    """Open an iterator over chunks of an HDF5 file"""
+    chunk_iter: TableIterator = pd.read_hdf(hdf5_path, key='raw_data', iterator=True, **kwargs)
+    try:
+        yield chunk_iter
+    finally:
+        if not isinstance(hdf5_path, HDFStore):
+            chunk_iter.close()
 
 
 def iterate_records_from_file(hdf5_path: Union[Path, str, HDFStore], **kwargs) -> Iterator[Dict[str, Union[str, float, int]]]:
@@ -16,14 +30,56 @@ def iterate_records_from_file(hdf5_path: Union[Path, str, HDFStore], **kwargs) -
         hdf5_path: Path to the data file
 
     Yields:
-        Individual rows from the "time-series"
+        Individual rows from the "raw_data" section of the HDF5 file
     """
 
-    chunk_iter: TableIterator = pd.read_hdf(hdf5_path, key='raw_data', iterator=True, **kwargs)
-    try:
+    with _get_raw_data_iterator_h5(hdf5_path, **kwargs) as chunk_iter:
         for chunk in chunk_iter:
             for _, row in chunk.iterrows():
-                yield row.to_dict()
-    finally:
-        if not isinstance(hdf5_path, HDFStore):
-            chunk_iter.close()
+                yield row
+
+
+def iterate_cycles_from_file(hdf5_path: Union[Path, str, HDFStore],
+                             make_dataset: bool = False,
+                             **kwargs) -> Iterator[Union[pd.DataFrame, BatteryDataset]]:
+    """Stream single-cycle datasets from the HDF5 file
+
+    Keyword arguments are passed to :meth:`~pandas.read_hdf`.
+
+    Args:
+        hdf5_path: Path to the data file
+        make_dataset: Whether to form a :class:`~batdata.data.BatteryDataset` for each cycle,
+            including the metadata from the source file.
+
+    Yields:
+        Individual cycles from the "time-series"
+    """
+
+    # Get the metadata out of the file, if needed
+    metadata = None
+    if make_dataset:
+        metadata, _ = BatteryDataset.inspect_batdata_hdf(hdf5_path)
+
+    def _assemble_from_chunks(chunks: List[pd.DataFrame]):
+        combined = pd.concat(chunks)
+        if make_dataset:
+            return BatteryDataset(metadata=metadata, raw_data=combined)
+        return combined
+
+    current_cycle = None
+    current_cycle_chunks = None
+    with _get_raw_data_iterator_h5(hdf5_path, **kwargs) as chunk_iter:
+        for new_chunk in chunk_iter:  # Until no more data
+            while not (cycle_mask := new_chunk['cycle_number'] == current_cycle).all():  # Loop until only current cycle
+                my_cycle = new_chunk[cycle_mask]  # Retrieve the parts from the current cycle
+                new_chunk = new_chunk[~cycle_mask]  # Remove those parts from the current chunk
+
+                # Yield the current data then reset the collection, increment cycle tracker
+                if current_cycle_chunks is not None:
+                    current_cycle_chunks.append(my_cycle)
+                    yield _assemble_from_chunks(current_cycle_chunks)
+                current_cycle_chunks = []
+                current_cycle = new_chunk['cycle_number'].iloc[0]
+            current_cycle_chunks.append(new_chunk)
+
+    yield _assemble_from_chunks(current_cycle_chunks)
--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,10 +1,13 @@
 """Evaluate streaming reads from files"""
+from itertools import zip_longest
 from pathlib import Path
 
+import numpy as np
 from pytest import fixture
 
+from batdata.data import BatteryDataset
 from batdata.extractors.batterydata import BDExtractor
-from batdata.streaming import iterate_records_from_file
+from batdata.streaming import iterate_records_from_file, iterate_cycles_from_file
 
 
 @fixture()
@@ -22,3 +25,17 @@ def test_stream_by_rows(example_h5_path):
     assert row_0['test_time'] == 0.
     row_1 = next(row_iter)
     assert row_1['voltage'] == 3.27191577
+
+
+def test_stream_by_cycles(example_h5_path):
+    test_data = BatteryDataset.from_batdata_hdf(example_h5_path)
+    cycle_iter = iterate_cycles_from_file(example_h5_path, chunksize=4)  # Small to make sure we split cycles across chunks
+    for streamed, (_, original) in zip_longest(cycle_iter, test_data.raw_data.groupby('cycle_number')):
+        assert streamed is not None
+        assert original is not None
+        assert np.allclose(streamed['test_time'], original['test_time'])
+
+    # Ensure we can generate chunks with metadata
+    cycle_iter = iterate_cycles_from_file(example_h5_path, make_dataset=True)
+    cycle_0 = next(cycle_iter)
+    assert cycle_0.metadata == test_data.metadata
