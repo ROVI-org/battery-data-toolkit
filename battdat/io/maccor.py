33597009f61dbe325f93f9262390f8ff06dfeb4e
lward <lward@anl.gov>
2025-05-23 13:39:05 -0400

    Add checks to ensure files are ordered by date
    Also allow parsing the datetime column,
    and skipping if it is problematic

--- battdat/io/maccor.py
+++ battdat/io/maccor.py
@@ -1,16 +1,24 @@
 """Extractor for MACCOR"""
+import re
 import itertools
-from typing import Union, List, Iterator, Tuple
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Union, List, Iterator, Tuple, Sequence, Optional
 
 import pandas as pd
 import numpy as np
 
-from battdat.io.base import DatasetFileReader, CycleTestReader
+from battdat.data import BatteryDataset
+from battdat.io.base import DatasetFileReader, CycleTestReader, PathLike
+from battdat.schemas import BatteryMetadata
 from battdat.schemas.column import ChargingState
 from battdat.postprocess.tagging import AddMethod, AddSteps, AddSubSteps
 from battdat.utils import drop_cycles
 
+_test_date_re = re.compile(r'Date of Test:\s+(\d{2}/\d{2}/\d{4})')
 
+
+@dataclass
 class MACCORReader(CycleTestReader, DatasetFileReader):
     """Parser for reading from MACCOR-format files
 
@@ -19,6 +27,9 @@ class MACCORReader(CycleTestReader, DatasetFileReader):
     the same prefix (i.e., everything except the numerals in the extension)
     are treated as part of the same experiment.
     """
+
+    ignore_time: bool = False
+    """Ignore the the time column, which can be problematic."""
 
     def group(self, files: Union[str, List[str]], directories: List[str] = None,
               context: dict = None) -> Iterator[Tuple[str, ...]]:
@@ -35,8 +46,29 @@ class MACCORReader(CycleTestReader, DatasetFileReader):
         for prefix, group in itertools.groupby(split_filenames, key=lambda x: x[0]):
             yield tuple('.'.join(x) for x in group)
 
-    def read_file(self, file: str, file_number: int = 0, start_cycle: int = 0,
+    def read_dataset(self, group: Sequence[PathLike] = (), metadata: Optional[BatteryMetadata] = None) -> BatteryDataset:
+        # Verify the cells are ordered by test date
+        start_dates = []
+        for file in group:
+            with open(file, 'r') as fp:
+                header = fp.readline()
+                test_date = _test_date_re.findall(header)[0]
+                start_dates.append(datetime.strptime(test_date, '%m/%d/%Y'))
+
+        # Make sure they are in the correct order
+        if not all(x >= y for x, y in zip(start_dates[1:], start_dates)):
+            msg = "\n  ".join(f'- {x} {y.strftime("%m/%d/%Y")}' for x, y in zip(group, start_dates))
+            raise ValueError(f'Files are not in the correct order by test date: {msg}\n')
+
+        return super().read_dataset(group, metadata)
+
+    def read_file(self, file: PathLike, file_number: int = 0, start_cycle: int = 0,
                   start_time: int = 0) -> pd.DataFrame:
+
+        # Pull the test date from the first line of the file
+        with open(file, 'r') as fp:
+            header = fp.readline()
+        test_date = _test_date_re.findall(header)[0]
 
         # Read in the ASCII file (I found this notation works)
         df = pd.read_csv(file, skiprows=1, engine='python', sep='\t', index_col=False, encoding="ISO-8859-1")
@@ -53,12 +85,21 @@ class MACCORReader(CycleTestReader, DatasetFileReader):
         df_out['state'] = df['State']
         df_out['current'] = df['Amps']
         df_out['current'] = np.where(df['State'] == 'D', -1 * df_out['current'], df_out['current'])
+
+        if not self.ignore_time:
+            def _parse_time(time: str) -> float:
+                if '/' in time:
+                    return datetime.strptime(time, '%m/%d/%Y %H:%M:%S').timestamp()
+                else:
+                    return datetime.strptime(f'{test_date} {time}', '%m/%d/%Y %H:%M:%S').timestamp()
+
+            df_out['time'] = df['DPt Time'].apply(_parse_time)
+
         #   0 is rest, 1 is charge, -1 is discharge
         df_out.loc[df_out['state'] == 'R', 'state'] = ChargingState.hold
         df_out.loc[df_out['state'] == 'C', 'state'] = ChargingState.charging
         df_out.loc[df_out['state'] == 'D', 'state'] = ChargingState.discharging
-        df_out.loc[df_out['state'] == 'O', 'state'] = ChargingState.unknown
-        df_out.loc[df_out['state'] == 'S', 'state'] = ChargingState.unknown
+        df_out.loc[df_out['state'].apply(lambda x: x not in {'R', 'C', 'D'}), 'state'] = ChargingState.unknown
 
         df_out['voltage'] = df['Volts']
         df_out = drop_cycles(df_out)
--- tests/io/test_maccor.py
+++ tests/io/test_maccor.py
@@ -1,5 +1,6 @@
 """Tests related to the MACCOR parser"""
-from pytest import fixture
+from datetime import datetime
+from pytest import fixture, raises
 
 from battdat.io.maccor import MACCORReader
 
@@ -30,3 +31,28 @@ def test_grouping(extractor, tmp_path):
     assert len(groups) == 2
     assert (str(tmp_path / 'testA.001'), str(tmp_path / 'testA.002')) in groups
     assert (str(tmp_path / 'testB.001'),) in groups
+
+
+def test_date_check(extractor, test_file):
+    files = [test_file, test_file.with_suffix('.002')]
+    data = extractor.read_dataset(files)
+    data.validate()
+    assert data.raw_data['file_number'].max() == 1
+
+    with raises(ValueError, match='not in the correct order'):
+        extractor.read_dataset(files[::-1])
+
+
+def test_time_parser(extractor, test_file):
+    # With date and time in the time column
+    df = extractor.read_file(test_file)
+    assert datetime.fromtimestamp(df['time'].iloc[0]).month == 3
+
+    # With only the time in the time column
+    df = extractor.read_file(test_file.with_suffix('.002'))
+    assert datetime.fromtimestamp(df['time'].iloc[0]).month == 4
+
+    # Ignoring datetime
+    extractor.ignore_time = True
+    df = extractor.read_file(test_file)
+    assert 'time' not in df.columns
